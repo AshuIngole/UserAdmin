6c8518edf68acdbbbbf041767c92fb512b0055f2
Admin <Admin@DESKTOP-QMFU2H6>
2023-12-08 17:29:33 +0530

    test commit for user admin

---
+++ UserAdminProject/src/main/java/admin_user/UserAdminApplication.java
@@ -0,0 +1,13 @@
+package admin_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAdminApplication.class, args);
+	}
+
+}
